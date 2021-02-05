ca01d279c7b2d80118375bef60591c71b2170be7
RHU5 <gusdnr814@naver.com>
2021-02-06 02:26:06 +0900

    babel & nodemon install

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 // require 함수는 express 폴더를 찾는다.
-const express = require("express");
+// const express = require("express");
+import express from "express";
 // express application을 만든다.
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
-}
+
+const handleHome = (req, res) => res.send("hi it's my home");
+const handleProfile = (req, res) => res.send("it's profile");
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
