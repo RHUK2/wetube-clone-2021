c0bf5323823a88cc5174f99ae88c447ab45a8e56
RHU5 <gusdnr814@naver.com>
2021-02-12 16:47:28 +0900

    other controller update

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,7 +2,7 @@ import express from "express";
 import {
   changePassword,
   editProfile,
-  userDetail,
+  userDetail
 } from "../controllers/userController";
 import routes from "../routes";
 
@@ -10,6 +10,6 @@ const userRouter = express.Router();
 
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
-userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
